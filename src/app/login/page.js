f11ae568bbfbe34e56ea981a4075f8a606c8026b
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-07-26 13:36:00 +0530

    connect to database and create Signup & login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,53 @@
-export default function loginPage() {
+'use client';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+export default function LoginPage() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const onLogin = async () => {};
   return (
-    <div>
-      <h1>This is the Login Page</h1>
+    <div className='d-flex flex-column justify-content-center align-items-center'>
+      <h1 className='mb-3'>Login</h1>
+      <Form className='mb-3'>
+        <Form.Group className='mb-3' controlId='email'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type='email'
+            value={user.email}
+            placeholder='Enter email'
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            value={user.password}
+            placeholder='Enter password'
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+        </Form.Group>
+        <Button
+          className='me-1'
+          variant='primary'
+          type='submit'
+          onClick={onLogin}
+        >
+          Signup here!
+        </Button>
+        <Link href='/signup'>
+          <Button type='button' variant='outline-dark'>
+            Visit Signup Page
+          </Button>
+        </Link>
+      </Form>
     </div>
   );
 }
--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -1,7 +1,63 @@
-export default function signupPage() {
+'use client';
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+
+export default function SignupPage() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    username: '',
+  });
+
+  const onSignup = async () => {};
   return (
-    <div>
-      <h1>This is signup page</h1>
+    <div className='d-flex flex-column justify-content-center align-items-center'>
+      <h1 className='mb-3'>Signup</h1>
+      <Form className='mb-3'>
+        <Form.Group className='mb-3' controlId='username'>
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type='text'
+            value={user.username}
+            placeholder='Enter Username'
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='email'>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type='email'
+            value={user.email}
+            placeholder='Enter email'
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+        </Form.Group>
+        <Form.Group className='mb-3' controlId='password'>
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type='password'
+            value={user.password}
+            placeholder='Enter password'
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
+        </Form.Group>
+        <Button
+          className='me-1'
+          variant='primary'
+          type='submit'
+          onClick={onSignup}
+        >
+          Signup here!
+        </Button>
+        <Link href='/login'>
+          <Button type='button' variant='outline-dark'>
+            Visit Login Page
+          </Button>
+        </Link>
+      </Form>
     </div>
   );
 }
