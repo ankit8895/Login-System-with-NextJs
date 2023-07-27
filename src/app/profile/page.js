52e464ec952c5d971555a5f1108ed064c454b7f6
ankit8895 <125978383+ankit8895@users.noreply.github.com>
2023-07-27 15:06:37 +0530

    implement authorization using custom middleware and implement helper to get user data

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,4 +1,5 @@
 'use client';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import axios from 'axios';
@@ -6,6 +7,7 @@ import { Button } from 'react-bootstrap';
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState('');
   const logout = async () => {
     try {
       await axios.get('/api/users/logout');
@@ -14,12 +16,28 @@ export default function ProfilePage() {
       console.log(error.message);
     }
   };
+
+  const getUserDetails = async () => {
+    const res = await axios.get('/api/users/me');
+    setData(res.data.data._id);
+  };
   return (
     <div className='d-flex flex-column justify-content-center align-items-center'>
       <h1 className='mb-3'>Profile</h1>
       <p>Profile Page</p>
+      <h2>
+        {data === '' ? (
+          'No user data'
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <Button variant='danger' className='mt-3' onClick={logout}>
         Logout
+      </Button>
+
+      <Button variant='success' className='mt-3' onClick={getUserDetails}>
+        GetUser Details
       </Button>
     </div>
   );
